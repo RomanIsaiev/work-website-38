6699a45e43c17d01db69064241d4afa80a8660f4
Roman Isaiev <107892291+RomanIsaiev@users.noreply.github.com>
2024-04-11 12:06:05 +0300

    Update faq.js

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,13 +1,13 @@
 import $ from 'jquery';
 
-$('.question').click(function () {
-  $(this).find('.content').toggle(200); //скрытие, показ ответа
-  if ($(this).hasClass('open')) {
-    $(this).removeClass('open');
-  } else {
-    $(this).addClass('open');
-  }
-});
+// $('.question').click(function () {
+//   $(this).find('.content').toggle(200); //скрытие, показ ответа
+//   if ($(this).hasClass('open')) {
+//     $(this).removeClass('open');
+//   } else {
+//     $(this).addClass('open');
+//   }
+// });
 
 $('.question-two').click(function () {
   $(this).find('.content-two').toggle(200); //скрытие, показ ответа
